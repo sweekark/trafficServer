a3d6ad58acef391072952d639a40005a6ec8cb60
sweekar <sweekar07@yahoo.com>
2013-05-26 09:43:50 +0530

    fixed normalizer added renderer

--- normalizer/normalizer.rb
+++ normalizer/normalizer.rb
@@ -9,35 +9,66 @@ include Mongo
 @normalizer = @db['normalizer']
 @normalizer.remove
 
+
+
+=begin
+  use the start point and end point from the uploader
+  insert it into the normalizer
+=end
+
+def normalizer(startPoint,endPoint)
+  timeDiff = endPoint["timestamp"] - startPoint["timestamp"]
+  @timeTakenCount = 0
+  @timeTakenLatest = 5
+  if ( @normalizerEntry = @normalizer.find_one( 
+                                               {:from=>startPoint["junctionId"],:to => endPoint["junctionId"]}
+                                              ) ) then
+                                              puts @normalizerEntry
+                                              @timeTakenArray = @normalizerEntry["timeTaken"]
+                                              @timeTakenCount =  @normalizerEntry["timeTaken"].count
+                                              @timeTakenLatest =  @normalizerEntry["latest"]
+  end
+  ## if the entry for the uvid is not present in normalizer create new
+  ## if present just push the value to the array 
+  ## upsert => true option will handle this
+  if @timeTakenCount < 5 then 
+    @normalizer.update(
+      {:from=>startPoint["junctionId"],:to => endPoint["junctionId"]},
+      {
+      "$push" => {:timeTaken =>  {:value => timeDiff,:timestamp => endPoint["timestamp"] }  
+    },"$set" => {:latest =>@timeTakenLatest}},
+      {
+      :upsert => true
+    })
+  else
+    @normalizer.update(
+      {:from=>startPoint["junctionId"],:to => endPoint["junctionId"]},
+      {"$set" => 
+        {"timeTaken.#{@timeTakenLatest}" =>  {:value => timeDiff,:timestamp => endPoint["timestamp"] },
+          "latest" => @timeTakenLatest}}
+    )
+
+  end
+end
+
 ## get the latest endpoints from uploader
-puts "new entries updated into uploader  {@coll.count()}"
-puts @coll.count()
+puts "new entries updated into uploader  #{@coll.count()}"
 count = 0
+
 @coll.find({"update"=>0}).each { 
   |endPoint|
   puts count 
+  if count == 6 
+    exit
+  end
   ## on each of these endpoints get the previous entry for the same uvid 
   ## this will be the start point
   startPoint = @coll.find_one(
     {"uvid" => endPoint["uvid"],
       "junctionId"=> {"$ne" => endPoint["junctionId"]}}
   )
-  timeDiff = endPoint["timestamp"] - startPoint["timestamp"]
-
-  ## if the entry for the uvid is not present in normalizer create new
-  if (@normalizer.find(:from=>startPoint["junctionId"],:to => endPoint["junctionId"]).count == 0) then
-    @normalizer.insert(:from=>startPoint["junctionId"],:to => endPoint["junctionId"],
-                       :timeTaken => [{:value => timeDiff,:timestamp => endPoint["timestamp"] } ] )
-  end
-
-  ## if present just push the value to the array
-  if (@normalizer.find(:from=>startPoint["junctionId"],:to => endPoint["junctionId"]).count != 0) then
-    @normalizer.update(
-      {:from=>startPoint["junctionId"],:to => endPoint["junctionId"]},
-      {
-      "$push" => {:timeTaken =>  [{:value => timeDiff,:timestamp => endPoint["timestamp"] } ] 
-    }
-    })
-  end
+  normalizer(startPoint,endPoint)
   count = count +1
 }
+
+
